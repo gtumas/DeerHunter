bbe941842d00623d2a90212453b6a361a1520e23
Andrius Kubilius <kubilius@mailinator.com>
2017-03-28 22:41:00 +0300

    Changed label text

--- DeerHunter/ViewController.swift
+++ DeerHunter/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = "Ohoho"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
